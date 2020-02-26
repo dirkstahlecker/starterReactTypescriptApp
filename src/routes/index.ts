f83865c941a882c9c64c8e066beba23cdb1a7522
Dirk Stahlecker <dirkstahlecker@Dirks-MacBook-Pro.local>
2020-02-26 01:13:19 -0500

    sending email is working

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 const fetch = require("node-fetch");
+const nodemailer = require("nodemailer");
 
 // Init router and path
 const router = Router();
 
 // Add sub-routes
 // router.use('/users', UserRouter);
+
 
 router.get('/getWeather', async (req, res) => {
 	console.log("in index.ts /getWeather");
@@ -14,10 +16,44 @@ router.get('/getWeather', async (req, res) => {
   const response_raw = await fetch(url);
   const response = await response_raw.json();
 
-  console.log(response);
-  console.log(response["currently"]["temperature"]);
+  const currentTemp = response["currently"]["temperature"];
 
-  return res.json({"response": response});
+
+
+
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    auth: {
+        user: 'sendmailprogrammatically@gmail.com',
+        pass: 'rockswitchjar'
+    }
+});
+
+	var mailOptions = {
+	  from: 'dirkstahlecker24@gmail.com',
+	  to: 'dirkstahlecker24@gmail.com',
+	  subject: 'Sending Email using Node.js',
+	  text: 'That was easy!'
+	};
+
+	transporter.sendMail(mailOptions, (error: any, info: any) => {
+	  if (error) {
+	    console.log(error);
+	  } else {
+	    console.log('Email sent: ' + info.response);
+	  }
+	});
+
+
+
+
+
+
+
+  return res.json({"currentTemp": currentTemp});
 });
 
 // Export the base-router
